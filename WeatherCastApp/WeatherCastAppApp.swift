96b6c4f51818fb436faf530a4fcb1739bbf2466f
mayar1711 <mayarhassan1711@gmail.com>
2024-05-12 17:57:52 +0300

    Initial Commit

---
+++ WeatherCastApp/WeatherCastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastAppApp.swift
+//  WeatherCastApp
+//
+//  Created by Mayar on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
